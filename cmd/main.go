c4d37668608fb3c9abb53a5e6be48839b37ab292
ka4oks1 <167628059+ka4oks1@users.noreply.github.com>
2026-01-23 00:10:23 +0300

    first response and write to file

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,46 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
+
+// main bot file
 
 func main() {
 
-	fmt.Println("Hey man i am bot")
-	client := &http.Client{}
+	resp, err := http.Get("https://google.com")
+
+	if err != nil {
+
+		fmt.Println("Something went wrong")
+
+	}
+
+	defer resp.Body.Close()
+
+	for true {
+
+		bs := make([]byte, 1014)
+		n, err := resp.Body.Read(bs)
+
+		str := fmt.Sprintln(string(bs[:n]))
+
+		fmt.Println(string(bs[:n]))
+
+		file, fileErr := os.OpenFile("../HTTP_req.txt", os.O_RDWR, 0644)
+		file.WriteString(str + "\n")
+
+		if fileErr != nil {
+			fmt.Println("fileErr")
+		}
+
+		defer file.Close()
+		if n == 0 || err != nil {
+			break
+		}
+	}
+	//fmt.Println("Hey man i am bot")
+
+	//client := &http.Client{}
 
 }
---
+++ internal/adapters/mymemory.go
@@ -0,0 +1,5 @@
+package mymemory
+
+func mymem() {
+
+}
